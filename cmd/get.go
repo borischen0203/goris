130ea474a6c35d6bd4c8beaf2fdcefaf2de2a327
Boris Chen <boris@Boris-Chen.local>
2021-08-29 17:39:09 +0800

    ADD. add command file

---
+++ cmd/get.go
@@ -0,0 +1,96 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+)
+
+// getCmd represents the get command
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "This command will get the desired Gopher",
+	Long:  `This get command will call GitHub repository in order to return the desired Gopher.`,
+	Args:  cobra.RangeArgs(1, 2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var gopherName = "dr-who.png"
+
+		path, _ := homedir.Dir()
+		path += "/desktop/"
+
+		if len(args) == 1 {
+			gopherName = args[0]
+			fmt.Println(gopherName)
+		}
+		if len(args) == 2 {
+			gopherName = args[0]
+			path = args[1] + "/"
+			fmt.Println(path)
+		}
+
+		URL := "https://github.com/scraly/gophers/raw/main/" + gopherName + ".png"
+
+		fmt.Println("Try to get '" + gopherName + "' Gopher...")
+
+		// Get the data
+		response, err := http.Get(URL)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode == 200 {
+			// Create the file
+			out, err := os.Create(path + gopherName + ".png")
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer out.Close()
+
+			// Writer the body to file
+			_, err = io.Copy(out, response.Body)
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+			fmt.Println("Perfect! Just saved in " + out.Name() + "!")
+		} else {
+			fmt.Println("Error: " + gopherName + " not exists! :-(")
+			return err
+		}
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/borischen0203/goris
+
+go 1.16
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/mitchellh/go-homedir v1.0.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cobra v1.2.1 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
