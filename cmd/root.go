df66edc3857d9a873c083ae5e89b040d0530104c
Boris Chen <boris@Boris-Chen.local>
2021-09-07 12:32:48 +0100

    UPD. comment

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,6 @@
+//Package cmd provides command for using
+package cmd
+
 /*
 Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 
@@ -13,9 +16,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-//Package cmd provides command for using
-package cmd
 
 import (
 	"fmt"
